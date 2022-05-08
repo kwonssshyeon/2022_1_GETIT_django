3b2feeac6978a744759d45d830d75aa674cf1fb3
kwonsh <kwonsh4767@naver.com>
2022-05-08 23:37:56 +0900

    FBV로 블ㄹ로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns =[
     path('',views.index),
+    path('<int:pk>/',views.single_post_page), #정수 형태의 값을 pk에 담아 single_post_page의 함수로 처리
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,14 @@ def index(request):
             'posts': posts,
         }
     )
+# Post.object.get에 pk대입 ->같은 값을 가져오는 의미 값이 html에 렌더링
+def single_post_page(request,pk):
+    post=Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post':post,
+        }
+    )    
