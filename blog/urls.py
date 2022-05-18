278382a84d8deab1a5074d0a7009f80024de8893
kwonsh <kwonsh4767@naver.com>
2022-05-18 16:47:58 +0900

    카테고리 페이지 만들기 완료

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns =[
     #path('',views.index),
     #path('<int:pk>/',views.single_post_page), #정수 형태의 값을 pk에 담아 single_post_page의 함수로 처리
+    path('category/<str:slug>/',views.category_page),
     path('',views.PostList.as_view()),
     path('<int:pk>/',views.PostDetail.as_view()),
 ]
